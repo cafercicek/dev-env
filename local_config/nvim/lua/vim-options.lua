e6a71a1779ae9182225c635348631bed9380a0f3
cafercicek <68570963+cafercicek@users.noreply.github.com>
2025-04-08 12:19:42 +0200

    Update vim-options.lua

--- local_config/nvim/lua/vim-options.lua
+++ local_config/nvim/lua/vim-options.lua
@@ -8,6 +8,7 @@ vim.cmd("set clipboard=unnamedplus")
 --vim.cmd("set mouse=a")
 vim.cmd("set number")
 vim.g.mapleader = " "
+--vim.cmd("tnoremap <Esc> <C-\\><C-n>")
 
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = "*",
@@ -18,3 +19,16 @@ vim.keymap.set('t', '<C-d>', [[<C-\><C-n>:q<CR>]], { noremap = true })
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
 
 vim.keymap.set('n', '<C-d>', ':q<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>sh', ':split<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>sv', ':vsplit<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>+', ':resize +2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>-', ':resize -2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>>', ':vertical resize +5<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader><', ':vertical resize -5<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true })
+vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true })
+vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true })
+vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true })
