a62943d262aa7c70f2415bf5f421e64fcb8d63f0
cafercicek <68570963+cafercicek@users.noreply.github.com>
2025-04-08 12:19:17 +0200

    Update neo-tree.lua

--- local_config/nvim/lua/plugins/neo-tree.lua
+++ local_config/nvim/lua/plugins/neo-tree.lua
@@ -1,11 +1,23 @@
-return { "nvim-neo-tree/neo-tree.nvim",
-    	branch = "v3.x", dependencies = {
-	"nvim-lua/plenary.nvim",
-	"nvim-tree/nvim-web-devicons",
-	"MunifTanjim/nui.nvim",},
-	config = function()
-		vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
---		vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal top<CR>')
-		vim.keymap.set('n', '<C-l>', ':Neotree toggle<CR>')
-	end
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+    vim.keymap.set('n', '<C-l>', ':Neotree toggle<CR>')
+
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true, 
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    })
+  end,
 }
