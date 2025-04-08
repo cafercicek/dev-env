d959f6274aa3928c00a9d80caa43054c121ac5fa
cafercicek <68570963+cafercicek@users.noreply.github.com>
2025-04-08 10:34:35 +0200

    Update vim-options.lua

--- local_config/nvim/lua/vim-options.lua
+++ local_config/nvim/lua/vim-options.lua
@@ -8,3 +8,11 @@ vim.cmd("set clipboard=unnamedplus")
 --vim.cmd("set mouse=a")
 vim.cmd("set number")
 vim.g.mapleader = " "
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  command = "startinsert"
+})
+vim.keymap.set('n', '<leader>tt', ':belowright 10split | terminal<CR>', { noremap = true, silent = true })
+vim.keymap.set('t', '<C-d>', [[<C-\><C-n>:q<CR>]], { noremap = true })
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
