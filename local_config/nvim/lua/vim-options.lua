0e3209b605b6ce4c2efd4132ff2bea46b3899976
cafercicek <68570963+cafercicek@users.noreply.github.com>
2025-04-08 11:52:50 +0200

    Update vim-options.lua

--- local_config/nvim/lua/vim-options.lua
+++ local_config/nvim/lua/vim-options.lua
@@ -16,3 +16,5 @@ vim.api.nvim_create_autocmd("TermOpen", {
 vim.keymap.set('n', '<leader>tt', ':belowright 10split | terminal<CR>', { noremap = true, silent = true })
 vim.keymap.set('t', '<C-d>', [[<C-\><C-n>:q<CR>]], { noremap = true })
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
+
+vim.keymap.set('n', '<C-d>', ':q<CR>', { noremap = true, silent = true })
