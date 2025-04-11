0842b5e06ac151d170775af813f79b46dabac044
cafercicek <68570963+cafercicek@users.noreply.github.com>
2025-04-11 17:13:27 +0200

    Update vim-options.lua

--- local_config/nvim/lua/vim-options.lua
+++ local_config/nvim/lua/vim-options.lua
@@ -23,8 +23,8 @@ vim.keymap.set('n', '<C-d>', ':q<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>sh', ':split<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>sv', ':vsplit<CR>', { noremap = true, silent = true })
 
-vim.keymap.set('n', '<leader>+', ':resize +2<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>-', ':resize -2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>+', ':resize +4<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>-', ':resize -4<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>>', ':vertical resize +5<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader><', ':vertical resize -5<CR>', { noremap = true, silent = true })
 
@@ -32,3 +32,14 @@ vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true })
 vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true })
 vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true })
 vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true })
+
+
+vim.keymap.set('n', '<leader><CR>', function()
+  local curr_win = vim.api.nvim_get_current_win()
+  vim.cmd("wincmd j")
+  if vim.api.nvim_get_current_win() == curr_win then
+    vim.cmd("wincmd k")
+  end
+end, { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>cr', ':%s/\\r//g<CR>', { noremap = true, silent = true })
